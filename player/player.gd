e363ca220c0d85882424db4db8a33c60216dd69b
polimer-consumer <jenyhlevnoy@gmail.com>
2024-03-21 20:57:45 +0200

    Player animation template

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
-const SPEED: int = 200  
+@export var speed: int = 200
+@onready var animation_player = $AnimationPlayer
 
 func  _ready():
 	add_to_group("player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_pressed("right") || Input.is_action_pressed("left") || Input.is_action_pressed("forward") || Input.is_action_pressed("backward"):
+		animation_player.play("walk")
+	else:
+		animation_player.play("idle")
 	var direction = Input.get_vector("left", "right", "forward", "backward")
-	velocity = direction * SPEED
+	velocity = direction * speed
 	move_and_slide()
