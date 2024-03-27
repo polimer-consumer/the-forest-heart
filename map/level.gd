7b3275b8c88f12d213724bceba13629bc7c2415e
masha <vichuganovam@gmail.com>
2024-03-27 20:54:05 +0200

    Lake
    enter and exit, take water

--- map/level.gd
+++ map/level.gd
@@ -1,7 +1,10 @@
 extends Node2D
-
+signal water_collected()
+signal player_near_water()
+signal player_exited_water()
 @onready var ambient_sound = $AudioStreamPlayer
 @onready var npc = $NPC
+var player_is_near = false
 
 func _ready():
 	ambient_sound.play()
@@ -13,4 +16,26 @@ func _on_npc_player_near_npc(body):
 
 
 func _on_trash_trash_collected():
-	print(" У тебя естьведро")
+	print(" У тебя есть ведро")
+
+
+func _on_player_entered_water(body):
+	if body.is_in_group("player"):
+		print("наберите воду")
+		player_is_near = true
+		player_near_water.emit()
+		
+func _on_player_exited_water(body):
+	if body.is_in_group("player"):
+		player_is_near = false
+		player_exited_water.emit()
+		
+func _process(delta):
+	_player_take_water()
+			
+func _player_take_water():		
+	if Input.is_action_just_pressed("take_water") and player_is_near:
+		print( "you have water now")
+		water_collected.emit()
+	
+	
