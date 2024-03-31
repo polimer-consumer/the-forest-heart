9caa48773f22eae8b68bad52375397da6b620ca2
masha <vichuganovam@gmail.com>
2024-04-01 01:30:26 +0300

    Dialogue when meet frog

--- Dialogue.gd
+++ Dialogue.gd
@@ -2,19 +2,43 @@ extends CanvasLayer
 
 @export_file("*.json") var d_file
 var dialogue =[]
+var cur_dialogue_id = 0
+var d_active = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start()
+	$NinePatchRect.visible = false
+	
+		
 func start():
+	if d_active:
+		return
+	d_active = true
+	$NinePatchRect.visible = true
 	dialogue = load_dialogue()
-	$NinePatchRect/Name.text = dialogue[0]['name']
-	$NinePatchRect/Chat.text = dialogue[4]['text']
+	cur_dialogue_id = -1
+	next_script()
 	
 func load_dialogue():
 	if FileAccess.file_exists(d_file):
 		var file = FileAccess.open(d_file, FileAccess.READ)
 		return JSON.parse_string(file.get_as_text())
 		
+func _input(event):
+	if not d_active:
+		return
+	if event.is_action_pressed("dialog"):
+		next_script()
+
+func next_script():
+	cur_dialogue_id += 1
 	
+	if cur_dialogue_id >= len(dialogue):
+		d_active = false
+		$NinePatchRect.visible = false
+		return
+	
+	$NinePatchRect/Name.text = dialogue[cur_dialogue_id]['name']
+	$NinePatchRect/Chat.text = dialogue[cur_dialogue_id]['text']
+
 	
 	
