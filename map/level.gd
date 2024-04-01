4e87641484d75382c27166ae91b50667313032f6
polimer-consumer <jenyhlevnoy@gmail.com>
2024-04-01 22:44:11 +0300

    Add trash collection and lake sound

--- map/level.gd
+++ map/level.gd
@@ -2,23 +2,23 @@ extends Node2D
 signal water_collected()
 signal player_near_water()
 signal player_exited_water()
-@onready var ambient_sound = $AudioStreamPlayer
+@onready var ambient_sound = $AmbientForestSound
+@onready var lake_sound = $TileMap/Area2D/LakeSound
 @onready var npc = $NPC
 var player_is_near = false
 var fire_scene = preload("res://bigfire/big_fire.tscn")
 
 func _ready():
 	ambient_sound.play()
+	lake_sound.play()
 
 func _on_npc_player_near_npc(body):
 	if body.is_in_group("player"):
 		print("player entered")
 
-
-
 func _on_trash_trash_collected():
-	print(" У тебя есть ведро")
-
+	remove_child($trash)
+	print("У тебя есть ведро")
 
 func _on_player_entered_water(body):
 	if body.is_in_group("player"):
@@ -42,5 +42,3 @@ func _player_take_water():
 	if Input.is_action_just_pressed("take_water") and player_is_near:
 		print( "you have water now")
 		water_collected.emit()
-	
-	
